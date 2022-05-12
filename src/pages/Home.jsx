1bd053af4a87f828d608b8aa3eed983f163e075e
nwachiii <egreatness1@gmail.com>
2022-05-12 17:53:33 +0100

    :sparkles: feat: set up pages, build login and home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,70 @@
+import {useIdleTimer} from 'react-idle-timer';
+import React, {useEffect, useState} from 'react';
+import {Alert, AlertIcon, VStack} from '@chakra-ui/react';
+import {useLocation, useNavigate} from 'react-router-dom';
+
+import {Footer, Header, UserDetails} from '../components';
+import { timeout, promptTimeout, onActive} from '../helpers';
+
+export const Home = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const username = location && location.state.username;
+	const [remaining, setRemaining] = useState(0);
+
+	const handleLogout = () => {
+		navigate('/', {replace: null});
+	};
+	const handleStillHere = () => {
+		window.location.reload(false);
+	};
+
+	const onIdle = () => handleLogout();
+	const onPrompt = () => {
+		return (
+			<Alert maxW={480} borderRadius='md' status='warning'>
+				<AlertIcon />
+				Hi {username}, Looks like you've been out of touch! You'll be logged out in about 20seconds from now`
+			</Alert>
+		);
+	};
+
+	const {getRemainingTime, getTotalIdleTime, isPrompted, getTotalActiveTime} = useIdleTimer({
+		timeout,
+		promptTimeout,
+		onPrompt,
+		onIdle,
+		onActive
+	});
+
+	useEffect(
+		() => {
+			const interval = setInterval(() => {
+				if (isPrompted()) {
+					setRemaining(Math.ceil(getRemainingTime() / 1000));
+				}
+			}, 6000);
+			return () => {
+				clearInterval(interval);
+			};
+		},
+		[isPrompted]
+	);
+
+	return (
+		<VStack h='100vh' flexDirection='column' mx='auto' justifyContent='center'>
+			<Header />
+			<UserDetails
+				onIdle={onIdle}
+				onPrompt={onPrompt}
+				username={username}
+				handleStillHere={handleStillHere}
+				isPrompted={isPrompted}
+				handleLogout={handleLogout}
+				getTotalIdleTime={getTotalIdleTime}
+				getTotalActiveTime={getTotalActiveTime}
+			/>
+			<Footer />
+		</VStack>
+	);
+};
---
+++ src/pages/Login.jsx
@@ -0,0 +1,92 @@
+import {
+	Center,
+	Input,
+	VStack,
+	FormLabel,
+	InputGroup,
+	FormControl,
+	useColorMode,
+	InputLeftElement,
+	useColorModeValue,
+	Image,
+	Text,
+	Button
+} from '@chakra-ui/react';
+import {FaRegUser} from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
+import {ToggleThemeIcon} from '../components';
+import bitmamaLogo from '../assets/logo.svg';
+import {theme} from '../theme';
+
+export const Login = () => {
+	const navigate = useNavigate();
+	const [name, setName] = useState('');
+	const LocalStorageuserName = JSON.parse(localStorage.getItem('bitmama_UserName'));
+
+	const handleSignIn = () => {
+		navigate('/home', {state: {username: name}});
+		localStorage.setItem('bitmama_UserName', JSON.stringify(name));
+		sessionStorage.setItem('activeSession', JSON.stringify(name));
+	};
+
+	const handleInputChange = (e) => {
+		e.preventDefault();
+		setName(e.target.value);
+	};
+
+	useEffect(() => {
+		setName(LocalStorageuserName ? LocalStorageuserName : '');
+	}, []);
+
+	const color = useColorModeValue('white', 'gray.800');
+	const {colorMode} = useColorMode();
+
+	return (
+		<Center
+			h='100vh'
+			color={color}
+			bgGradient={colorMode === 'light' ? 'linear(to-br, black, teal.600)' : 'linear(to-tr, red.100, white)'}>
+			<ToggleThemeIcon />
+			<VStack spacing={4} align='stretch'>
+				<Center
+					mb={6}
+					flexDirection='column'
+					borderRadius={colorMode === 'light' ? '3xl' : 'full'}
+					boxShadow='rgba(0, 0, 0, 0.35) 0px 5px 15px'>
+					<Image boxSize='150px' src={bitmamaLogo} alt='Bitmama Logo' />
+					<Text pb={4} fontWeight='bold'>
+						USER LOGIN
+					</Text>
+				</Center>
+				<FormControl isRequired>
+					<FormLabel htmlFor='first-name'>Username</FormLabel>
+					<InputGroup>
+						<InputLeftElement pointerEvents='none' children={<FaRegUser color='gray.300' />} />
+						<Input
+							value={name}
+							onChange={handleInputChange}
+							variant='outline'
+							borderColor='gray.400'
+							placeholder='Username'
+						/>
+					</InputGroup>
+				</FormControl>
+				<Button
+					p={6}
+					color='#FFF'
+					borderRadius='md'
+					bg={theme.brand.blue}
+					onClick={handleSignIn}
+					_hover={{
+						bg          : '#FFF',
+						color       : '#03a438',
+						border : '2px solid #03a438',
+					}}>
+					Login
+				</Button>
+			</VStack>
+		</Center>
+	);
+};
