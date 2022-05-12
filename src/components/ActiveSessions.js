11e7d09af6755bf20103a42a0373a0c7b1164912
nwachiii <egreatness1@gmail.com>
2022-05-12 18:04:02 +0100

    feat( W.I.P ): The application is able to manage the sessions of all users

---
+++ src/components/ActiveSessions.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {MdCheckCircle} from 'react-icons/md';
+import {Box, Heading, List, ListIcon, ListItem, Text} from '@chakra-ui/react';
+import { CustomBox } from './CustomBox';
+
+export const ActiveSessions = () => {
+	// const activeSessions = sessionStorage.getItem('activeSession');
+	// const [activeSessionsStorage, setActiveSessionsStorage] = useState(['']);
+
+	// useEffect(
+	// 	() => {
+	// 		setActiveSessionsStorage(activeSessionsStorage.push(activeSessions));
+	// 	},
+	// 	[activeSessions]
+	// );
+
+	// console.log('activeSessions', activeSessions);
+	// console.log('activeSessionsStorage', activeSessionsStorage);
+
+	return (
+		<CustomBox>
+			<Text fontSize='24px' fontWeight='bold' as='p' bgClip='text' bgGradient='linear(to-l, #7928CA, #FF0080)' mb={4}>Other active users:</Text>
+			<List spacing={3}>
+				<ListItem>
+					<ListIcon as={MdCheckCircle} color='green.500' />
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit
+				</ListItem>
+				<ListItem>
+					<ListIcon as={MdCheckCircle} color='green.500' />
+					Assumenda, quia temporibus eveniet a libero incidunt suscipit
+				</ListItem>
+				<ListItem>
+					<ListIcon as={MdCheckCircle} color='green.500' />
+					Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+				</ListItem>
+			</List>
+		</CustomBox>
+	);
+};
